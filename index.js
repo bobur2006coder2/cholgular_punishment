2fb313afcc6c95173a09df1890989a26df4c70a7
bobur2006coder <boburdilmurodov702@gmail.com>
2023-06-12 03:46:48 +0500

    fix

---
+++ index.js
@@ -0,0 +1,21 @@
+let one = document.querySelector(".one")
+let two = document.querySelector(".two")
+let three = document.querySelector(".three")
+let four = document.querySelector(".four")
+
+one.addEventListener('click',()=>{
+    let playmusic=new Audio('Drums After Joke Punchline Sound Effect HD _ No Copyright.mp3')
+    playmusic.play()
+})
+two.addEventListener('click',()=>{
+    let playmusic=new Audio('Trumpet sound effect.mp3')
+    playmusic.play()
+})
+three.addEventListener('click',()=>{
+    let playmusic=new Audio('Cool Guitar Riff Off _ Sound Effect.mp3')
+    playmusic.play()
+})
+four.addEventListener('click',()=>{
+    let playmusic=new Audio('Piano Sound Effect.mp3')
+    playmusic.play()
+})
